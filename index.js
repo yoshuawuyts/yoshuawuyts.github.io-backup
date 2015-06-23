177caab57402b5c12a45e7b1f8f4693aa9777020
Yoshua Wuyts <i@yoshuawuyts.com>
2015-06-23 14:07:05 +0200

    router: add

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 require('webcomponents.js')
 
+const wayfarer = require('wayfarer')
+const match = require('hash-match')
+
+// register custom elements
 require('@about/background-video')()
-// document.registerElement('div', 'video-background', require('@about/video-background'))
+
+// register router
+const router = wayfarer('/')
+router.on('/', () => console.log('/'))
+router.on('/skills', () => console.log('skills'))
+router.on('/about', () => console.log('about'))
+router.on('/contact', () => console.log('contact'))
+
+router(match(window.location.hash))
+
+window.addEventListener('hashchange', () => {
+  router(match(window.location.hash))
+})
